9513781621e6a12ffababb284f2fc9314acd3a96
Kris Moore <kris@ixsystems.com>
2019-04-06 10:25:11 -0400

    Implement all the loading of submission json, and cleanup the formatting of the JSON submission

--- usage.go
+++ usage.go
@@ -6,9 +6,49 @@ import (
     "net/http"
 )
 
+// Global vars
+var SDIR="/var/db/ix-stats"
+var DAILYFILE string
+
+// Submission JSON structs
+//////////////////////////////////////////////////////////
+type s_plugins struct {
+    Name string
+    Version string
+}
+
+type s_pools struct {
+    Name string
+    Type string
+    Vdevs int
+    Disks int
+    Capacity int
+}
+
+type s_hw struct {
+    Cpus int
+    Memory int
+    Nics int
+}
+
+type s_services struct {
+    Name string
+    Enabled bool
+}
+
 type submission_json struct {
     Platform string
     Version string
+    Plugins []s_plugins `json:"plugins"`
+    Pools []s_plugins `json:"pools"`
+    Hardware s_hw `json:"hardware"`
+    Services []s_services `json:"services"`
+}
+
+//////////////////////////////////////////////////////////
+
+func load_daily_file() {
+
 }
 
 func submit(rw http.ResponseWriter, req *http.Request) {
@@ -16,9 +56,15 @@ func submit(rw http.ResponseWriter, req *http.Request) {
     var s submission_json
     err := decoder.Decode(&s)
     if err != nil {
+	log.Println(err)
         return
     }
     log.Println(s.Platform)
+    log.Println(s.Version)
+    log.Println(s.Plugins)
+    log.Println(s.Pools)
+    log.Println(s.Hardware)
+    log.Println(s.Services)
 }
 
 func main() {
